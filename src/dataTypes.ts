5f3b0c39a68607f75470ae2f142ecc587ccff738
nelikantimeenakshi6 <nelikantimeenakshi6@gmail.com>
2026-07-29 18:02:29 +0530

    update code

---
+++ src/dataTypes.ts
@@ -0,0 +1,21 @@
+let data: any = "Meenakshi";
+console.log(data);
+data = 500;
+console.log(data);
+
+let person: { name: string; age: number } = {
+  name: "Meena",
+  age: 30
+};
+console.log(person);
+
+let employee: {name: string, city: string; role: string, salary: number } =
+{
+    
+    name: "Meenakshi",
+    city: "Hyderabad",
+
+    role: "Senior IT",
+    salary: 500000
+};
+console.log(employee);
